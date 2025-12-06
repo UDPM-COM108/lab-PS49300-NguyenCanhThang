e685f7490c9ff01564edd91d21802e92b7d35f86
PS49300-NguyenCanhThang <canhthang457@gmail.com>
2025-12-06 22:59:41 +0700

    Update ASM2

--- ASM/ASM2.c
+++ ASM/ASM2.c
@@ -4,124 +4,170 @@
 #include <string.h>
 
 void chucnang1(){
-    int x;
+    printf("Nhap vo mot so: ");
+    int n;
     float temp;
-    printf("Nhap vao 1 so: ");
+    if(scanf("%f", &temp) != 1 || temp != (int)temp){
+        printf("%f khong phai la so nguyen\n", temp);
+        return;
+    }
+    n = (int)temp;
+    printf("%d la so nguyen\n", n);
+
+    if(n<2){
+        printf("%d khong phai la so nguyen to\n", n);
+        return;
+    }
+    int laNguyenTo = 1;
+    for(int i=2; i <= sqrt(n); i++){
+        if(n%i == 0){
+            laNguyenTo = 0;
+            break;
+        }    
+    }
+    if(laNguyenTo){
+        printf("%d la so nguyen to\n", n);
+    }else{
+        printf("%d khong phai la so nguyen to\n", n);
+    }
     
-    if(scanf("%f", &temp) != 1 || (int)temp != temp){
-        printf("\n%f khong phai la so nguyen", temp);
+    if(n<0){
+        printf("%d khong phai la so chinh phuong\n", n);
         return;
     }
-    x = (int)temp;
-    printf("\n%d la so nguyen", x);
-
-    if(x < 2){
-        printf("\n%d khong phai la so nguyen to", x);
-        return;
-    }
-    int dem = 1; // gan dinh la so nguyen to
-    for(int i=2; i <= sqrt(x); i++){
-        if(x%i == 0){
-            dem = 0;
-            break;
+    int laChinhPhuong = 0;
+    for(int i = 0; i <= sqrt(n); i++){
+        if(i*i == n){
+            laChinhPhuong = 1;
         }
     }
-    if(dem){
-        printf("\n%d la so nguyen to", x);
+    if(laChinhPhuong){
+        printf("%d la so chinh phuong\n", n);
     }else{
-        printf("\n%d khong phai la so nguyen to", x);
-    }
-
-    if(x < 0){
-        printf("\n%d khong phai la so chinh phuong", x);
-        return;
-    }
-    int chinhphuong = 0;
-    for(int i= 0; i <= sqrt(x); i++){
-        if(i*i == x){
-            chinhphuong = 1; //tim thay
-            break;
-        }
-    }
-    if(chinhphuong){
-        printf("\n%d la so chinh phuong", x);
-    }else{
-        printf("\n%d khong phai la so chinh phuong", x);
+        printf("%d khong phai la so chinh phuong\n", n);
     }
 }
 
 void chucnang2(){
-    int x,y,max=1,min;
-    printf("Nhap vao 2 so x y:");
-    scanf("%d %d",&x,&y);
-    //UCLN
-    if (x==0||y==0){
-        max=x+y;
+    printf("Nhap vao x va y: ");
+    int x, y;
+    scanf("%d %d", &x, &y);
+    int a = x, b = y;
+    while(b!=0){
+        int r = a % b;
+        a = b;
+        b = r;
     }
-    
-    for (int i = 1; i <= x && i<= y; i++){
-        if (x%i==0&&y%i==0){
-            if (max<i){
-                max=i;
-            }
-        }
+    int USCLN = a;
+    int BSCNN = (x * y) / USCLN;
+    printf("Uoc so chung lon nhat: %d\n", USCLN);
+    printf("Boi so chung nho nhat: %d", BSCNN);     
+}
+
+void chucnang3(){
+    int start, end;
+    printf("Nhap vao gio bat dau: ");
+    scanf("%d", &start);
+    printf("Nhap vao gio ket thuc: ");
+    scanf("%d", &end);
+    if(start < 12 || end > 23 || start >= end){
+        printf("Quan chi hoat dong tu 12 -> 23. Moi nhap lai!\n");
+        return;
     }
-    printf("UCLN la %d\n",max);
-    //BCNN
-    if (x>y){
-        min=x;
+    int soGio = end - start;
+    float soTien = 0;
+    if(soGio <= 3){
+        soTien = soGio * 150000;
     }else{
-        min=y;
+        soTien = 3 * 150000;
+        soTien += (soGio - 3) * 105000; // Giam 30% tu gio so 4
     }
-    while (1){// dk chay vo han
-        if (min%x==0&&min%y==0){
-            break;// khi tim dc bs chung nho nhat thi dung lai
-        }
-        min++;
+    if(start >= 14 && start <= 17){
+        soTien *= 0.9; // Giam 10% con 90%-> 0.9
     }
-    printf("BCNN la %d\n",min);
+    printf("Tong so tien phai thanh toan la: %.0f", soTien);
 }
 
 void chucnang4(){
-    int k,TIEN=0;
-    printf("Nhap vao so dien(kwh):");
-    scanf("%d",&k);
-    if (k<=50){
-        TIEN=k*1678;
+    printf("Nhap vao so kwh dien su dung: ");
+    int kwh;
+    scanf("%d", &kwh);
+    float tien = 0;
+    if(kwh < 0){
+        printf("Nhap khong hop le!");
+        return;
     }
-    else if (k<=100){
-        TIEN=50*1678;
-    	TIEN+=(k-50)*1734;
+    if(kwh <= 50){
+        tien = kwh * 1678;
+    }else if(kwh <= 100){
+        tien = 50 * 1678 
+             + (kwh - 50) * 1734;
+    }else if(kwh <= 200){
+        tien = 50 * 1678 
+             + 50 * 1734 
+             + (kwh - 100) * 2014;
+    }else if(kwh <= 300){
+        tien = 50 * 1678 
+             + 50 * 1734 
+             + 100 * 2014 
+             + (kwh - 200) * 2536;
+    }else if(kwh <= 400){
+        tien = 50 * 1678 
+             + 50 * 1734 
+             + 100 * 2014
+             + 100 * 2536
+             + (kwh - 300) * 2834;
+    }else{
+        tien = 50 * 1678 
+             + 50 * 1734 
+             + 100 * 2014
+             + 100 * 2536
+             + 100 * 2834 
+             + (kwh - 400) * 2927;
     }
-    else if (k<=200){
-        TIEN=50*1678;
-    	TIEN+=50*1734;
-    	TIEN+=(k-100)*2014;
-    }
-    else if (k<=300){
-        TIEN=50*1678;
-    	TIEN+=50*1734;
-    	TIEN+=100*2014;
-    	TIEN+=(k-200)*2536;
-    }
-    else if (k<=400){
-        TIEN=50*1678;
-    	TIEN+=50*1734;
-    	TIEN+=100*2014;
-    	TIEN+=200*2536;
-        TIEN+=(k-300)*2834;
-    }
-    else{
-        TIEN=50*1678;
-    	TIEN+=50*1734;
-    	TIEN+=100*2014;
-    	TIEN+=200*2536;
-        TIEN+=300*2834;
-        TIEN+=(k-400)*2927;
-    }
-    printf("TIEN DIEN = %d VND",TIEN);
+    printf("Tong so tien phai tra la: %.0f", tien);
 }
-   
+
+void chucnang5(){
+    printf("Nhap vao so tien can doi: ");
+    int tien;
+    scanf("%d", &tien);
+    int menhgia[] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+    int soTo;
+
+    printf("Ket qua doi tien: \n");
+    for(int i = 0; i < 9; i++){
+        soTo = tien / menhgia[i];
+        if(soTo > 0){
+            printf("%d to %d\n", soTo, menhgia[i]);
+        }
+        tien %= menhgia[i];
+    }
+}
+
+void chucnang6(){
+    printf("Nhap so tien ban muon vay: ");
+    float soTienVay = 0;
+    scanf("%f", &soTienVay);
+    if(soTienVay <= 0){
+        printf("Nhap khong hop le!");
+        return;
+    }
+    float laiSuatNH = 0.05; // 5% / thang
+    int soThang = 12;
+    float gocThang = soTienVay / soThang;
+    float noConLai = soTienVay;
+    
+    printf("\nBang tra gop 12 thang:\n");
+    printf("Thang\tGoc\t\tLai\t\tTong\t\tNo con lai\n");
+    
+    for(int i = 1; i <= soThang; i++){
+        float laiThang = noConLai * laiSuatNH;
+        float tongThang = gocThang + laiThang;
+        noConLai -= gocThang;
+        printf("%d\t%.0f\t\t%.0f\t\t%.0f\t\t%.0f\n", i, gocThang, laiThang, tongThang, noConLai);
+    }
+}
 
 int main(){
     int luachon;
@@ -150,18 +196,18 @@ int main(){
             case 2:{
                 chucnang2();
                 }break;
-            // case 3:{
-            //     chucnang3();
-            //     }break;
+            case 3:{
+                chucnang3();
+                }break;
             case 4:{
                 chucnang4();
                 }break;
-            // case 5:{
-            //     chucnang5();
-            //     }break;
-            // case 6:{
-            //     chucnang6();
-            //     }break;
+            case 5:{
+                chucnang5();
+                }break;
+            case 6:{
+                chucnang6();
+                }break;
             // case 7:{
             //     chucnang7();
             //     }break;
